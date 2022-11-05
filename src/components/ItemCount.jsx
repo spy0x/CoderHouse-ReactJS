6b51c0100b30b99e7ebd3184bbcd93a884a24b86
Francisco V. Diaz <fevarga1@uc.cl>
2022-11-05 15:33:57 -0300

    item count refined for itemlist

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -2,11 +2,10 @@ import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import React from "react";
 
-export default function ItemCount() {
-  const stock = 25;
+export default function ItemCount({ stock }) {
   function validateQuantity({ target }) {
     if (target.value > stock) target.value = stock;
-    else if (target.value < 0) target.value = 0;
+    else if (target.value < 0) target.value = 1;
   }
   return (
     <Box
@@ -23,15 +22,15 @@ export default function ItemCount() {
           id="select_quantity"
           label="Quantity"
           type="number"
-          defaultValue={0}
+          defaultValue={1}
           InputLabelProps={{
             shrink: true,
           }}
           InputProps={{
             inputProps: {
               max: stock,
-              min: 0,
-              style: { textAlign: 'center' }
+              min: 1,
+              style: { textAlign: "center" },
             },
           }}
           onChange={(e) => validateQuantity(e)}
