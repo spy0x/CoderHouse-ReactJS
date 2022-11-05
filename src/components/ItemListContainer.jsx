ae7c5067462834b28868be45733f15ef2f4df565
Francisco V. Diaz <fevarga1@uc.cl>
2022-11-05 11:14:38 -0300

    added contador to itemlistcontainer

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,16 @@
 import { Paper, Grid, Typography } from '@mui/material';
 import React from 'react';
+import ItemCount from './ItemCount';
 
 export default function ItemListContainer({ greeting }) {
   return (
-    <Grid container spacing={2} justifyContent="center" my={2}>
+    <Grid container spacing={2} justifyContent="center" my={2} alignItems="center">
       <Grid item xs={10} md={8}>
         <Paper elevation={4} sx={{bgcolor: 'whitesmoke'}}>
           <Typography variant="h4" color="initial" p={4} align='center'>
             {greeting}
           </Typography>
+          <ItemCount/>
         </Paper>
       </Grid>
     </Grid>
