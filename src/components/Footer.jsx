a0eaf7bb0864ea504c40b89b6af7de173764425e
Francisco V. Diaz <fevarga1@uc.cl>
2022-11-05 11:14:15 -0300

    fixed warning log in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { AppBar, BottomNavigation, Stack, Typography } from '@mui/material';
-import { grey } from '@mui/material/colors';
+import React from "react";
+import { AppBar, BottomNavigation, Stack, Typography } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 const navBarBackground = grey[900];
 
 export default function Footer() {
   return (
-    <AppBar position="fixed" sx={{ top: 'auto', bottom: 0 }}>
-      <BottomNavigation sx={{ backgroundColor: `${navBarBackground}`, height: '10vh' }}>
-        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
-          <Typography variant='subtitle2'>
-            © 2022 LTP - LOS TRES PRIMOS GAME STUDIO
-            </Typography>
-        </Stack>
-      </BottomNavigation>
+    <AppBar
+      position="fixed"
+      sx={{
+        backgroundColor: `${navBarBackground}`,
+        height: "10vh",
+        top: "auto",
+        bottom: 0,
+        justifyContent: "center",
+      }}
+    >
+      <Stack direction="row" justifyContent="center" alignItems="center" spacing={2}>
+        <Typography variant="subtitle2">© LTP - LOS TRES PRIMOS GAME STUDIO</Typography>
+      </Stack>
     </AppBar>
   );
 }
