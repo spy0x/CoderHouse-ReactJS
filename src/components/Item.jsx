91d87e0132f6446af85162df95af2654e5cdbdb0
Francisco V. Diaz <fevarga1@uc.cl>
2022-11-05 23:45:38 -0300

    added viewdetail button

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Divider, Stack } from "@mui/material";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -26,9 +26,23 @@ export default function Item({ product }) {
             {product.author}
           </Typography>
         </CardContent>
-        <Stack direction="row" justifyContent="space-between" mx={4} alignItems="center" spacing={2}>
-          <ItemCount stock={product.stock} />
-          <Button size="small" sx={{padding: "1rem"}}>Add to Cart</Button>
+        <Stack direction="column">
+          <Button
+            variant="contained"
+            color="error"
+            size="medium"
+            href="#contained-buttons"
+            sx={{ marginX: "4rem", marginBottom: "1rem", textAlign: "center" }}
+          >
+            View Details
+          </Button>
+          <Divider variant="middle" />
+          <Stack direction="row" justifyContent="space-between" mx={4} my={1} alignItems="center" spacing={2}>
+            <ItemCount stock={product.stock} />
+            <Button size="small" sx={{ padding: "1rem" }}>
+              Add to Cart
+            </Button>
+          </Stack>
         </Stack>
       </Stack>
     </Card>
