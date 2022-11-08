4debb366bbd53a61f87228cbd8ec216f233246bd
Francisco V. Diaz <fevarga1@uc.cl>
2022-11-07 23:47:17 -0300

    made itemdetail fully responsive

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -13,6 +13,7 @@ const boxStyle = {
 };
 
 export default function ItemDetail({ item }) {
+  const { img, title, author, specifications, price, stock } = item;
   return (
     <Box sx={boxStyle}>
       <Grid
@@ -24,25 +25,33 @@ export default function ItemDetail({ item }) {
         alignContent="center"
         wrap="wrap"
       >
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <CardMedia
             component="img"
             sx={{
               height: "70vh",
               objectFit: "contain",
             }}
-            src={item.img}
+            src={img}
           />
         </Grid>
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} sm={6}>
           <Stack direction="column" justifyContent="flex-start" alignItems="stretch" spacing={3}>
-            <Typography variant="h6" color="initial" textAlign="center">
-              {item.title}
-            </Typography>
-            <TableItemSpecs itemSpecs={item.specifications} />
-            <Divider/>
+            <Box>
+              <Typography variant="h6" color="initial" textAlign="center">
+                {title}
+              </Typography>
+              <Typography variant="body2" color="text.secondary" align="center">
+                {author}
+              </Typography>
+            </Box>
+            <TableItemSpecs itemSpecs={specifications} />
+              <Typography mt={2} display="block" variant="h6" color="text.secondary" align="center" gutterBottom>
+                ${price} USD
+              </Typography>
+            <Divider />
             <Stack direction="row" justifyContent="space-between" mx={4} my={1} alignItems="center" spacing={2}>
-              <ItemCount stock={item.stock} />
+              <ItemCount stock={stock} />
               <Button size="small" sx={{ padding: "1rem" }}>
                 Add to Cart
               </Button>
