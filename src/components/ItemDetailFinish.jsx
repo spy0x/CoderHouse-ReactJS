8300e29959a11773f7a0a2492b96d4438996c64b
Francisco V. Diaz <fevarga1@uc.cl>
2022-11-22 19:57:19 -0300

    created component with finish buying in itemdetail

---
+++ src/components/ItemDetailFinish.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Button, Stack } from "@mui/material";
+import ShoppingCartRoundedIcon from "@mui/icons-material/ShoppingCartRounded";
+import StoreRoundedIcon from '@mui/icons-material/StoreRounded';
+
+export default function ItemDetailFinish() {
+  return (
+    <Stack direction="row" justifyContent="center" alignItems="center" spacing={10}>
+      <Link to="/cart" style={{ textDecoration: "none" }}>
+        <Button
+          variant="contained"
+          color="error"
+          size="medium"
+          startIcon={<ShoppingCartRoundedIcon />}
+          sx={{ textAlign: "center" }}
+        >
+          View Cart
+        </Button>
+      </Link>
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <Button
+          variant="contained"
+          color="error"
+          size="medium"
+          startIcon={<StoreRoundedIcon />}
+          sx={{ textAlign: "center" }}
+        >
+          Continue Shopping
+        </Button>
+      </Link>
+    </Stack>
+  );
+}
