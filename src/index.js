9936d0ddcfdad9aa3450fed6ae2e750761c0fd00
Francisco V. Diaz <fevarga1@uc.cl>
2026-07-06 18:24:22 -0400

    security update

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCnKMjk--3F5M-lBncaQhFkOv5LxhCdcFA",
-  authDomain: "coderhouse-fvd.firebaseapp.com",
-  projectId: "coderhouse-fvd",
-  storageBucket: "coderhouse-fvd.appspot.com",
-  messagingSenderId: "252611736624",
-  appId: "1:252611736624:web:a84ec4518dabf0b5eb29f5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
